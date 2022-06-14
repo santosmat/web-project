179805aa13bd0897ef7a79ab16d3577cf7a26d24
FabioCavaleti <f.cavaleti@yahoo.com>
2022-06-14 15:42:32 -0300

    Carrinho finalizado (Faltou apenas a possibilidade de alterar a quantidade)

--- entrega02-clientside/src/Components/CartPage.js
+++ entrega02-clientside/src/Components/CartPage.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './CartPage.css'
+import foto from '../Assets/livroGenerico.jpg'
+import { Link } from 'react-router-dom';
 
 const CartPage = ({cart, setCart, deleteItem, clearCart}) => {
 
+    
+    
     const handleClickBuy = () => {
         window.alert("Finalizar compra")
         clearCart();
@@ -11,14 +15,52 @@ const CartPage = ({cart, setCart, deleteItem, clearCart}) => {
     const handleClickClear = () =>{
         clearCart();
     }
+    const calculaTot = () => {
+        
+        let sum = 0;
+        cart.map((item) =>{
+            sum += parseFloat(item.valor)
+        })
+        
+        return parseFloat(sum).toFixed(2);
+    }
+    
+    const [total, setTotal] = useState(calculaTot())
+
+    useEffect(() =>{
+        setTotal(calculaTot());
+    }, [cart])
+
+
+
     return ( 
         <div className='cart-container'>
             <h1>Carrinho</h1>
             <ul className='cart-list'>
                 {cart.map(item =>
-                    <li className='cart-item' key={item.id}>{item.titulo}</li>    
+                    <li className='cart-item' key={item.id}>
+                        <div className='left'>
+                            <Link to={`/bookpage/${item.id}`}>
+                                <img className='cart-product-img' alt='foto-do-livro' src={foto}/>
+                            </Link>
+                           <span>
+                             {`${item.titulo} `}
+                            </span>
+                        </div>
+                        <div className='right'>
+                            <span>
+                            {`R$${item.valor}`}
+                            </span>
+                            <button className='delete-item-btn' onClick={() =>{deleteItem(item)}}>X</button>
+
+                        </div>
+                    </li>    
                 )}
             </ul>
+
+            <div className='final-value'>
+                <span>{total > 0.0 ? `Total: R$${total}` : `o carrinho está vazio`}</span>
+            </div>
 
             <button onClick={handleClickBuy} className='buy-cart-btn btn'>Finalizar Compra</button>
                         <br />
